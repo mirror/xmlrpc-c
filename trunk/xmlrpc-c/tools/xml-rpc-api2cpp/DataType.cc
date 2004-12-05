c5f26a4b23c2bfba132992bba4411a5e9b2e100e
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2004-12-05 03:56:37 +0000

    Use modern sstream instead of strstream.h so it compiles on modern systems
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@174 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/tools/xml-rpc-api2cpp/DataType.cc
+++ trunk/xmlrpc-c/tools/xml-rpc-api2cpp/DataType.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <strstream.h>
+#include <sstream>
 #include <stdexcept>
 
 #include <XmlRpcCpp.h>
@@ -17,11 +17,9 @@ using std::domain_error;
 //  a specific XML-RPC data type.
 
 string DataType::defaultParameterBaseName (int position) const {
-    ostrstream name_stream;
+    ostringstream name_stream;
     name_stream << typeName() << position << ends;
     string name(name_stream.str());
-    // (Ask the ostrstream to reclaim ownership of its buffer.)
-    name_stream.freeze(false);
     return name;
 }
 
