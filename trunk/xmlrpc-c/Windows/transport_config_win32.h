a4b708d22c3776e4936c561ed23b8e8f159ebf2a
sbone <sbone@98333e67-4a24-44d7-a75c-e53540dd3050>
2005-03-28 05:13:13 +0000

    Addition made for easier use of curl.  If curl is set to be built, the pragmas added in this header will tell the linker what libraries it needs.
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@276 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/Windows/transport_config_win32.h
+++ trunk/xmlrpc-c/Windows/transport_config_win32.h
@@ -2,3 +2,19 @@
 #define MUST_BUILD_CURL_CLIENT 0
 #define MUST_BUILD_LIBWWW_CLIENT 0
 static const char * const XMLRPC_DEFAULT_TRANSPORT = "wininet";
+
+/*
+We use pragma statements to tell the linker what we need to link with.
+Since Curl requires Winsock, Winmm, and libcurl, and no other
+project does, if we are building curl support we tell the linker
+what libs we need to add.
+*/
+#if MUST_BUILD_CURL_CLIENT > 0
+#ifdef _DEBUG
+#pragma comment( lib, "../lib/libcurld.lib" )
+#else
+#pragma comment( lib, "../lib/libcurl.lib" )
+#endif
+#pragma comment( lib, "Winmm.lib" )
+#pragma comment( lib, "Ws2_32.lib" )
+#endif
