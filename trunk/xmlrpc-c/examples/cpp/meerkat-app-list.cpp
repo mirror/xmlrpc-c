762d5e6b0cffe83547c41eaf8ccd6558e48da6ad
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2005-11-23 22:28:10 +0000

    fix NUL character in time_period string
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@528 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/examples/cpp/meerkat-app-list.cpp
+++ trunk/xmlrpc-c/examples/cpp/meerkat-app-list.cpp
@@ -15,12 +15,13 @@ using namespace std;
 #define MEERKAT_URL    "http://www.oreillynet.com/meerkat/xml-rpc/server.php"
 #define SOFTWARE_LINUX (6)
 
-static void list_apps (int hours) {
+static void
+list_apps(unsigned int const hours) {
 
     // Build our time_period parameter.
     ostringstream time_period_stream;
-    time_period_stream << hours << "HOUR" << ends;
-    string time_period = time_period_stream.str();
+    time_period_stream << hours << "HOUR";
+    string const time_period(time_period_stream.str());
 
     // Assemble our meerkat query recipe.
     XmlRpcValue recipe = XmlRpcValue::makeStruct();
@@ -29,64 +30,60 @@ static void list_apps (int hours) {
     recipe.structSetValue("descriptions", XmlRpcValue::makeInt(76));
 
     // Build our parameter array.
-    XmlRpcValue param_array = XmlRpcValue::makeArray();
+    XmlRpcValue param_array(XmlRpcValue::makeArray());
     param_array.arrayAppendItem(recipe);
 
     // Create a client pointing to Meerkat.
-    XmlRpcClient meerkat (MEERKAT_URL);
+    XmlRpcClient meerkat(MEERKAT_URL);
 
     // Perform the query.
-    XmlRpcValue apps = meerkat.call("meerkat.getItems", param_array);
+    XmlRpcValue const apps(meerkat.call("meerkat.getItems", param_array));
 
     // Print our results.
-    int first = 1;
-    size_t app_count = apps.arraySize();
-    for (size_t i = 0; i < app_count; i++) {
-	XmlRpcValue app = apps.arrayGetItem(i);
+    size_t const app_count(apps.arraySize());
+    bool first;
+    size_t i;
+    for (i = 0, first=false; i < app_count; ++i, first = false) {
+        XmlRpcValue const app(apps.arrayGetItem(i));
 
-	// Get some information about our application.
-	string title       = app.structGetValue("title").getString();
-	string link        = app.structGetValue("link").getString();
-	string description = app.structGetValue("description").getString();
-	
-	// Print a separator line if necessary.
-	if (first)
-	    first = 0;
-	else
-	    cout << endl;
+        // Get some information about our application.
+        string const title(app.structGetValue("title").getString());
+        string const link(app.structGetValue("link").getString());
+        string const description(
+            app.structGetValue("description").getString());
+    
+        // Print a separator line if necessary.
+        if (!first)
+            cout << endl;
 
-	// Print this application entry.
-	if (description.size() > 0) {
-	    cout << title << endl << description << endl << link << endl;
-	} else {
-	    cout << title << endl << description << endl << link << endl;
-	}
+        // Print this application entry.
+        if (description.size() > 0)
+            cout << title << endl << description << endl << link << endl;
+        else
+            cout << title << endl << description << endl << link << endl;
     }
 }
 
-// Print out a usage message.
-static void usage (void)
-{
-    cerr << "Usage: meekat-app-list [hours]" << endl;
-    cerr << "Data from <http://www.oreillynet.com/meerkat/>." << endl;
-    exit(1);
-}
 
-int main (int argc, char **argv) {
-    int status = 0;
-    int hours = 25;
+
+int
+main(int argc, char **argv) {
+    int retval;
+    unsigned int hours;
 
     // Parse our command-line arguments.
     if (argc == 1) {
-	// Use default value for hours.
+        hours = 25;
     } else if (argc == 2) {
-	hours = atoi(argv[1]);
+        hours = atoi(argv[1]);
     }
-    if (hours == 0)
-	usage();
+    if (hours == 0) {
+        cerr << "Hours must be positive" << endl;
+        exit(1);
+    }
     if (hours > 49) {
         cerr << "It's not nice to ask for > 49 hours at once." << endl;
-        exit(1);	
+        exit(1);    
     }
 
     // Start up our client library.
@@ -94,15 +91,16 @@ int main (int argc, char **argv) {
 
     // Call our implementation, and watch out for faults.
     try {
-	list_apps(hours);
+        list_apps(hours);
+        retval = 0;
     } catch (XmlRpcFault& fault) {
-	cerr << argv[0] << ": XML-RPC fault #" << fault.getFaultCode()
-	     << ": " << fault.getFaultString() << endl;
-	status = 1;
+        cerr << argv[0] << ": XML-RPC fault #" << fault.getFaultCode()
+             << ": " << fault.getFaultString() << endl;
+        retval = 1;
     }
 
     // Shut down our client library.
     XmlRpcClient::Terminate();
 
-    return status;
+    return retval;
 }
