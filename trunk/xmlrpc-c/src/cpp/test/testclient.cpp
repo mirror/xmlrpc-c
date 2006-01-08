0b0bd7c35054df36f1866a8643eed8cd16b1a259
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2006-01-08 21:13:46 +0000

    resort code
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@575 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/src/cpp/test/testclient.cpp
+++ trunk/xmlrpc-c/src/cpp/test/testclient.cpp
@@ -28,6 +28,195 @@ using girerr::error;
 using namespace xmlrpc_c;
 using namespace std;
 
+
+
+class sampleAddMethod : public method {
+public:
+    sampleAddMethod() {
+        this->_signature = "ii";
+        this->_help = "This method adds two integers together";
+    }
+    void
+    execute(xmlrpc_c::paramList const& paramList,
+            value *             const  retvalP) {
+        
+        int const addend(paramList.getInt(0));
+        int const adder(paramList.getInt(1));
+        
+        paramList.verifyEnd(2);
+        
+        *retvalP = value_int(addend + adder);
+    }
+};
+
+
+
+class carriageParm_direct : public carriageParm {
+public:
+    carriageParm_direct(registry * const registryP) : registryP(registryP) {}
+
+    registry * registryP;
+};
+
+
+class clientXmlTransport_direct : public clientXmlTransport {
+
+public:    
+    void
+    call(xmlrpc_c::carriageParm * const  carriageParmP,
+         string                   const& callXml,
+         string *                 const  responseXmlP) {
+
+        carriageParm_direct * const parmP =
+            dynamic_cast<carriageParm_direct *>(carriageParmP);
+
+        if (parmP == NULL)
+            throw(error("Carriage parameter passed to the direct "
+                        "transport is not type carriageParm_direct"));
+
+        parmP->registryP->processCall(callXml, responseXmlP);
+    }
+};
+
+
+
+class clientDirectAsyncTestSuite : public testSuite {
+/*----------------------------------------------------------------------------
+   See clientDirectTestSuite for a description of how we use a
+   clientXmlTransport_direct object to test client functions.
+
+   The object of this class tests the async client functions.  With
+   clientXmlTransport_direct, these are pretty simple because the
+   transport doesn't even implement an asynchronous interface; it
+   relies on the base class' emulation of start() using call().
+
+   Some day, we should add true asynchronous capability to
+   clientXmlTransport_direct and really test things.
+-----------------------------------------------------------------------------*/
+public:
+    virtual string suiteName() {
+        return "clientDirectAsyncTestSuite";
+    }
+    virtual void runtests(unsigned int const) {
+        
+        registry myRegistry;
+        
+        myRegistry.addMethod("sample.add", methodPtr(new sampleAddMethod));
+        
+        carriageParm_direct carriageParmDirect(&myRegistry);
+        clientXmlTransport_direct transportDirect;
+        client_xml clientDirect(&transportDirect);
+        paramList paramListSampleAdd1;
+        paramListSampleAdd1.add(value_int(5));
+        paramListSampleAdd1.add(value_int(7));
+        paramList paramListSampleAdd2;
+        paramListSampleAdd2.add(value_int(30));
+        paramListSampleAdd2.add(value_int(-10));
+
+        rpcPtr const rpcSampleAdd1P("sample.add", paramListSampleAdd1);
+        rpcSampleAdd1P->start(&clientDirect, &carriageParmDirect);
+        rpcPtr const rpcSampleAdd2P("sample.add", paramListSampleAdd2);
+        rpcSampleAdd2P->start(&clientDirect, &carriageParmDirect);
+        
+        TEST(rpcSampleAdd1P->isFinished());
+        TEST(rpcSampleAdd1P->isSuccessful());
+        value_int const result1(rpcSampleAdd1P->getResult());
+        TEST(static_cast<int>(result1) == 12);
+        
+        TEST(rpcSampleAdd2P->isFinished());
+        TEST(rpcSampleAdd1P->isSuccessful());
+        value_int const result2(rpcSampleAdd2P->getResult());
+        TEST(static_cast<int>(result2) == 20);
+
+        EXPECT_ERROR(clientDirect.finishAsync(timeout()););
+        EXPECT_ERROR(clientDirect.finishAsync(timeout(50)););
+    }
+};
+
+
+
+class clientDirectTestSuite : public testSuite {
+/*----------------------------------------------------------------------------
+  The object of this class tests the client facilities by using a
+  special client XML transport defined above and an XML-RPC server we
+  build ourselves and run inline.  We build the server out of a
+  xmlrpc_c::registry object and our transport just delivers XML
+  directly to the registry object and gets the response XML from it
+  and delivers that back.  There's no network or socket or pipeline or
+  anything -- the transport actually executes the XML-RPC method.
+-----------------------------------------------------------------------------*/
+public:
+    virtual string suiteName() {
+        return "clientDirectTestSuite";
+    }
+    virtual void runtests(unsigned int const indentation) {
+        registry myRegistry;
+        
+        myRegistry.addMethod("sample.add", methodPtr(new sampleAddMethod));
+        
+        carriageParm_direct carriageParmDirect(&myRegistry);
+        clientXmlTransport_direct transportDirect;
+        client_xml clientDirect(&transportDirect);
+        paramList paramListSampleAdd;
+        paramListSampleAdd.add(value_int(5));
+        paramListSampleAdd.add(value_int(7));
+        paramList paramListEmpty;
+        {
+            /* Test a successful RPC */
+            rpcPtr rpcSampleAddP("sample.add", paramListSampleAdd);
+            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
+            TEST(rpcSampleAddP->isFinished());
+            TEST(rpcSampleAddP->isSuccessful());
+            value_int const resultDirect(rpcSampleAddP->getResult());
+            TEST(static_cast<int>(resultDirect) == 12);
+        }
+        {
+            /* Test a failed RPC */
+            rpcPtr const rpcSampleAddP("sample.add", paramListEmpty);
+            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
+            TEST(rpcSampleAddP->isFinished());
+            TEST(!rpcSampleAddP->isSuccessful());
+            fault const fault0(rpcSampleAddP->getFault());
+            TEST(fault0.getCode() == fault::CODE_TYPE);
+        }
+
+        {
+            /* Test with an auto object transport */
+            client_xml clientDirect(
+                clientXmlTransportPtr(new clientXmlTransport_direct));
+            rpcPtr rpcSampleAddP("sample.add", paramListSampleAdd);
+            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
+            TEST(rpcSampleAddP->isFinished());
+            TEST(rpcSampleAddP->isSuccessful());
+            EXPECT_ERROR(fault fault0(rpcSampleAddP->getFault()););
+            value_int const resultDirect(rpcSampleAddP->getResult());
+            TEST(static_cast<int>(resultDirect) == 12);
+        }
+        {
+            /* Test with implicit RPC -- success */
+            rpcOutcome outcome;
+            clientDirect.call(&carriageParmDirect, "sample.add",
+                              paramListSampleAdd, &outcome);
+            TEST(outcome.succeeded());
+            value_int const result(outcome.getResult());
+            TEST(static_cast<int>(result) == 12);
+        }
+        {
+            /* Test with implicit RPC - failure */
+            rpcOutcome outcome;
+            clientDirect.call(&carriageParmDirect, "nosuchmethod",
+                              paramList(), &outcome);
+            TEST(!outcome.succeeded());
+            TEST(outcome.getFault().getCode() == fault::CODE_NO_SUCH_METHOD);
+            TEST(outcome.getFault().getDescription().size() > 0);
+        }
+
+        clientDirectAsyncTestSuite().run(indentation+1);
+    }
+};
+
+
+
 class curlTransportTestSuite : public testSuite {
 
 public:
@@ -200,193 +389,6 @@ public:
     }
 };
         
-
-
-class carriageParm_direct : public carriageParm {
-public:
-    carriageParm_direct(registry * const registryP) : registryP(registryP) {}
-
-    registry * registryP;
-};
-
-
-class clientXmlTransport_direct : public clientXmlTransport {
-
-public:    
-    void
-    call(xmlrpc_c::carriageParm * const  carriageParmP,
-         string                   const& callXml,
-         string *                 const  responseXmlP) {
-
-        carriageParm_direct * const parmP =
-            dynamic_cast<carriageParm_direct *>(carriageParmP);
-
-        if (parmP == NULL)
-            throw(error("Carriage parameter passed to the direct "
-                        "transport is not type carriageParm_direct"));
-
-        parmP->registryP->processCall(callXml, responseXmlP);
-    }
-};
-
-
-
-class sampleAddMethod : public method {
-public:
-    sampleAddMethod() {
-        this->_signature = "ii";
-        this->_help = "This method adds two integers together";
-    }
-    void
-    execute(xmlrpc_c::paramList const& paramList,
-            value *             const  retvalP) {
-        
-        int const addend(paramList.getInt(0));
-        int const adder(paramList.getInt(1));
-        
-        paramList.verifyEnd(2);
-        
-        *retvalP = value_int(addend + adder);
-    }
-};
-
-
-
-class clientDirectAsyncTestSuite : public testSuite {
-/*----------------------------------------------------------------------------
-   See clientDirectTestSuite for a description of how we use a
-   clientXmlTransport_direct object to test client functions.
-
-   The object of this class tests the async client functions.  With
-   clientXmlTransport_direct, these are pretty simple because the
-   transport doesn't even implement an asynchronous interface; it
-   relies on the base class' emulation of start() using call().
-
-   Some day, we should add true asynchronous capability to
-   clientXmlTransport_direct and really test things.
------------------------------------------------------------------------------*/
-public:
-    virtual string suiteName() {
-        return "clientDirectAsyncTestSuite";
-    }
-    virtual void runtests(unsigned int const) {
-        
-        registry myRegistry;
-        
-        myRegistry.addMethod("sample.add", methodPtr(new sampleAddMethod));
-        
-        carriageParm_direct carriageParmDirect(&myRegistry);
-        clientXmlTransport_direct transportDirect;
-        client_xml clientDirect(&transportDirect);
-        paramList paramListSampleAdd1;
-        paramListSampleAdd1.add(value_int(5));
-        paramListSampleAdd1.add(value_int(7));
-        paramList paramListSampleAdd2;
-        paramListSampleAdd2.add(value_int(30));
-        paramListSampleAdd2.add(value_int(-10));
-
-        rpcPtr const rpcSampleAdd1P("sample.add", paramListSampleAdd1);
-        rpcSampleAdd1P->start(&clientDirect, &carriageParmDirect);
-        rpcPtr const rpcSampleAdd2P("sample.add", paramListSampleAdd2);
-        rpcSampleAdd2P->start(&clientDirect, &carriageParmDirect);
-        
-        TEST(rpcSampleAdd1P->isFinished());
-        TEST(rpcSampleAdd1P->isSuccessful());
-        value_int const result1(rpcSampleAdd1P->getResult());
-        TEST(static_cast<int>(result1) == 12);
-        
-        TEST(rpcSampleAdd2P->isFinished());
-        TEST(rpcSampleAdd1P->isSuccessful());
-        value_int const result2(rpcSampleAdd2P->getResult());
-        TEST(static_cast<int>(result2) == 20);
-
-        EXPECT_ERROR(clientDirect.finishAsync(timeout()););
-        EXPECT_ERROR(clientDirect.finishAsync(timeout(50)););
-    }
-};
-
-
-
-class clientDirectTestSuite : public testSuite {
-/*----------------------------------------------------------------------------
-  The object of this class tests the client facilities by using a
-  special client XML transport defined above and an XML-RPC server we
-  build ourselves and run inline.  We build the server out of a
-  xmlrpc_c::registry object and our transport just delivers XML
-  directly to the registry object and gets the response XML from it
-  and delivers that back.  There's no network or socket or pipeline or
-  anything -- the transport actually executes the XML-RPC method.
------------------------------------------------------------------------------*/
-public:
-    virtual string suiteName() {
-        return "clientDirectTestSuite";
-    }
-    virtual void runtests(unsigned int const indentation) {
-        registry myRegistry;
-        
-        myRegistry.addMethod("sample.add", methodPtr(new sampleAddMethod));
-        
-        carriageParm_direct carriageParmDirect(&myRegistry);
-        clientXmlTransport_direct transportDirect;
-        client_xml clientDirect(&transportDirect);
-        paramList paramListSampleAdd;
-        paramListSampleAdd.add(value_int(5));
-        paramListSampleAdd.add(value_int(7));
-        paramList paramListEmpty;
-        {
-            /* Test a successful RPC */
-            rpcPtr rpcSampleAddP("sample.add", paramListSampleAdd);
-            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
-            TEST(rpcSampleAddP->isFinished());
-            TEST(rpcSampleAddP->isSuccessful());
-            value_int const resultDirect(rpcSampleAddP->getResult());
-            TEST(static_cast<int>(resultDirect) == 12);
-        }
-        {
-            /* Test a failed RPC */
-            rpcPtr const rpcSampleAddP("sample.add", paramListEmpty);
-            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
-            TEST(rpcSampleAddP->isFinished());
-            TEST(!rpcSampleAddP->isSuccessful());
-            fault const fault0(rpcSampleAddP->getFault());
-            TEST(fault0.getCode() == fault::CODE_TYPE);
-        }
-
-        {
-            /* Test with an auto object transport */
-            client_xml clientDirect(
-                clientXmlTransportPtr(new clientXmlTransport_direct));
-            rpcPtr rpcSampleAddP("sample.add", paramListSampleAdd);
-            rpcSampleAddP->call(&clientDirect, &carriageParmDirect);
-            TEST(rpcSampleAddP->isFinished());
-            TEST(rpcSampleAddP->isSuccessful());
-            EXPECT_ERROR(fault fault0(rpcSampleAddP->getFault()););
-            value_int const resultDirect(rpcSampleAddP->getResult());
-            TEST(static_cast<int>(resultDirect) == 12);
-        }
-        {
-            /* Test with implicit RPC -- success */
-            rpcOutcome outcome;
-            clientDirect.call(&carriageParmDirect, "sample.add",
-                              paramListSampleAdd, &outcome);
-            TEST(outcome.succeeded());
-            value_int const result(outcome.getResult());
-            TEST(static_cast<int>(result) == 12);
-        }
-        {
-            /* Test with implicit RPC - failure */
-            rpcOutcome outcome;
-            clientDirect.call(&carriageParmDirect, "nosuchmethod",
-                              paramList(), &outcome);
-            TEST(!outcome.succeeded());
-            TEST(outcome.getFault().getCode() == fault::CODE_NO_SUCH_METHOD);
-            TEST(outcome.getFault().getDescription().size() > 0);
-        }
-
-        clientDirectAsyncTestSuite().run(indentation+1);
-    }
-};
-
 
 
 class clientCurlTestSuite : public testSuite {
