c6241165cea753a0b143d801d2947e21e4196b7f
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2006-07-29 21:45:19 +0000

    Fix version: 1.06
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@709 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/lib/abyss/src/abyss_info.h
+++ trunk/xmlrpc-c/lib/abyss/src/abyss_info.h
@@ -1,9 +1,9 @@
 #ifndef ABYSS_INFO_H_INCLUDED
 #define ABYSS_INFO_H_INCLUDED
 
-#define SERVER_VERSION      "1.05"
+#define SERVER_VERSION      "1.06"
 
-#define SERVER_HVERSION     "XMLRPC_ABYSS/1.05"
+#define SERVER_HVERSION     "XMLRPC_ABYSS/1.06"
 
 #define SERVER_HTML_INFO \
   "<p><HR><b><i><a href=\"http:\057\057xmlrpc-c.sourceforge.net\">" \
