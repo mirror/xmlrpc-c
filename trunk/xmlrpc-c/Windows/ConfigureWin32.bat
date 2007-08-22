63486ec3daccb886d763faf1239cc80bbcf43c01
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2007-08-22 02:39:03 +0000

    Do <xmlrpc-c/config.h> properly
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@990 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/xmlrpc-c/Windows/ConfigureWin32.bat
+++ trunk/xmlrpc-c/Windows/ConfigureWin32.bat
@@ -1,6 +1,6 @@
 @echo off
 echo creating Win32 header files...
-copy .\win32_config.h ..\config.h
+copy .\win32_config.h ..\include\xmlrpc-c\config.h
 copy .\xmlrpc_win32_config.h ..\xmlrpc_config.h
 copy .\transport_config_win32.h ..\transport_config.h
 echo completed creating win32 header files.
---
+++ trunk/xmlrpc-c/Windows/win32_config.h
@@ -0,0 +1,9 @@
+/* This file, part of XML-RPC For C/C++, is meant to 
+   define characteristics of this particular installation 
+   that the other <xmlrpc-c/...> header files need in 
+   order to compile correctly when #included in a Xmlrpc-c
+   user code.
+
+   Those header files #include this one.
+*/
+#define XMLRPC_HAVE_WCHAR 0
