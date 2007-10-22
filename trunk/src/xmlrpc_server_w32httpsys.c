bcc0a5a35b681e9a6f5c63974da8a0dfd1ed1a9c
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2007-10-22 05:52:01 +0000

    fix comment grammar, move copyright crap
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@1169 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/src/xmlrpc_server_w32httpsys.c
+++ trunk/src/xmlrpc_server_w32httpsys.c
@@ -1,28 +1,4 @@
-/* Copyright (C) 2005 by Steven A. Bone, sbone@pobox.com. All rights reserved.
-**
-** Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions
-** are met:
-** 1. Redistributions of source code must retain the above copyright
-**    notice, this list of conditions and the following disclaimer.
-** 2. Redistributions in binary form must reproduce the above copyright
-**    notice, this list of conditions and the following disclaimer in the
-**    documentation and/or other materials provided with the distribution.
-** 3. The name of the author may not be used to endorse or promote products
-**    derived from this software without specific prior written permission. 
-**  
-** THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-** ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-** OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-** HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-** LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-** OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-** SUCH DAMAGE. */
-
+/* Copyright information is at end of file. */
 /* COMPILATION NOTE:
    Note that the Platform SDK headers and
    link libraries for Windows XP SP2 or newer are required to compile
@@ -506,7 +482,7 @@ DoReceiveRequests(
                         ].RawValueLength] = '\0';
                     if (_stricmp(szHeaderBuf,"text/xml")!=0)
                     {
-                        //We only handle text/xml data.  Anything else
+                        //We handle only text/xml data.  Anything else
                         //is not valid.
                         TraceW(L"POST request had an unrecognized "
                                "content-type: %s", szHeaderBuf);
@@ -600,7 +576,7 @@ DoReceiveRequests(
                     break;
 
                 default:
-                    //We only handle POST data.  Anything else is not valid.
+                    //We handle only POST data.  Anything else is not valid.
                     TraceW(L"Got an unrecognized Verb request for URI %ws",
                            pRequest->CookedUrl.pFullUrl);
             
@@ -999,3 +975,31 @@ Done:
 
     return;
 }
+
+
+
+/* Copyright (C) 2005 by Steven A. Bone, sbone@pobox.com. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+** 3. The name of the author may not be used to endorse or promote products
+**    derived from this software without specific prior written permission. 
+**  
+** THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+** ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+** OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+** HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+** LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+** OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+** SUCH DAMAGE. */
+
