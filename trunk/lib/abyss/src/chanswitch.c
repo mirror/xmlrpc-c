16fd4b43d460cf774b4c9eae231388034e8502fe
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2007-11-14 21:52:39 +0000

    fix comment
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@1236 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/lib/abyss/src/chanswitch.c
+++ trunk/lib/abyss/src/chanswitch.c
@@ -71,7 +71,7 @@ ChanSwitchTerm(void) {
 
 
 
-/* SwitchCreate() is not exported to the Abyss user.  It is meant to
+/* ChanSwitchCreate() is not exported to the Abyss user.  It is meant to
    be used by an implementation-specific TChanSwitch generator which is
    exported to the Abyss user, e.g. SwitchCreateUnix() in
    socket_unix.c
