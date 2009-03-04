988485a71aabf4e0beca9f27d1f8b958c3dde2db
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2009-03-04 03:57:20 +0000

    Add invocation example comment
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@1661 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/examples/xmlrpc_socket_server.c
+++ trunk/examples/xmlrpc_socket_server.c
@@ -7,7 +7,11 @@
    creates the socket itself.  Also see xmlrpc_inetd_server.c, which is
    the same thing except you give it a socket which is already connected
    to a client.
- */
+
+   Example:
+
+   $ socketexec -local_port=8080 ./xmlrpc_socket_server
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
