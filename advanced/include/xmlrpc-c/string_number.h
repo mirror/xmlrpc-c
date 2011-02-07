f08612aec985ee80bfa1f74009180a01f9fe2609
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2011-02-07 01:11:08 +0000

    Release 1.25.03
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2084 98333e67-4a24-44d7-a75c-e53540dd3050

--- advanced/include/xmlrpc-c/string_number.h
+++ advanced/include/xmlrpc-c/string_number.h
@@ -4,9 +4,18 @@
 #include <xmlrpc-c/config.h>
 #include <xmlrpc-c/util.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+XMLRPC_DLLEXPORT
 void
 xmlrpc_parse_int64(xmlrpc_env *   const envP,
                    const char *   const str,
                    xmlrpc_int64 * const i64P);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
