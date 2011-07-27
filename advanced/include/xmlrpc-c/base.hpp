c0bc5028cf48f138d766b2d890d1c90afc9df3f6
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2011-07-27 15:29:13 +0000

    Release 1.27.02
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2163 98333e67-4a24-44d7-a75c-e53540dd3050

--- advanced/include/xmlrpc-c/base.hpp
+++ advanced/include/xmlrpc-c/base.hpp
@@ -226,28 +226,6 @@ public:
 
 
 
-template<class InputIterator> xmlrpc_c::value_array
-arrayValueSlice(InputIterator begin,
-                InputIterator end) {
-/*----------------------------------------------------------------------------
-  convert C++ iterator pair to XML-RPC array
------------------------------------------------------------------------------*/
-    carray ret;
-    for (InputIterator p = begin; p != end; ++p) {
-        ret.push_back(toValue(*p));
-    }
-    return xmlrpc_c::value_array(ret);
-}
-
-template<class MemberClass> inline xmlrpc_c::value_array
-arrayValueArray(const MemberClass * const in,
-                size_t              const size) {
-/*----------------------------------------------------------------------------
-  convert C++ array to XML-RPC array
------------------------------------------------------------------------------*/
-    return arrayValueSlice(in, in + size);
-}
-
 class XMLRPC_DLLEXPORT value_nil : public value {
 public:
     value_nil();
@@ -296,7 +274,7 @@ toValue(double const x) {
 }
 
 inline xmlrpc_c::value_bytestring
-    toValue(cbytestring const& x) {
+toValue(cbytestring const& x) {
     return xmlrpc_c::value_bytestring(x);
 }
 
@@ -322,6 +300,19 @@ toValue(std::map<K, V> const& in) {
         ret[p->first] = toValue(p->second);
     }
     return xmlrpc_c::value_struct(ret);
+}
+
+template<class InputIterator> xmlrpc_c::value_array
+arrayValueSlice(InputIterator begin,
+                InputIterator end) {
+/*----------------------------------------------------------------------------
+  convert C++ iterator pair to XML-RPC array
+-----------------------------------------------------------------------------*/
+    carray ret;
+    for (InputIterator p = begin; p != end; ++p) {
+        ret.push_back(toValue(*p));
+    }
+    return xmlrpc_c::value_array(ret);
 }
 
 template<class T> inline xmlrpc_c::value_array
@@ -396,6 +387,15 @@ fromValue(std::vector<T> & y, xmlrpc_c::value const& x) {
     for (unsigned int i = 0; i < v.size(); ++i) {
         fromValue(y[i], v[i]);
     }
+}
+
+template<class MemberClass> inline xmlrpc_c::value_array
+arrayValueArray(const MemberClass * const in,
+                size_t              const size) {
+/*----------------------------------------------------------------------------
+  convert C++ array to XML-RPC array
+-----------------------------------------------------------------------------*/
+    return arrayValueSlice(in, in + size);
 }
 
 class XMLRPC_DLLEXPORT fault {
