16ef8ab98e9e4e5c88aff1c084dd74be706c667e
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2012-04-26 02:37:03 +0000

    Fix another winsock2 conflict
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2311 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/include/xmlrpc-c/girmem.hpp
+++ trunk/include/xmlrpc-c/girmem.hpp
@@ -1,3 +1,24 @@
+/*============================================================================
+                               girmem.hpp
+==============================================================================
+  This declares the user interface to memory management facilities (smart
+  pointers, basically) in libxmlrpc.  They are used in interfaces to various
+  classes in XML For C/C++.
+  
+  This file includes <windows.h>, and does it with the WIN32_LEAN_AND_MEAN
+  switch so as not to interfere with other Xmlrpc-c header files that might
+  be included in the same program.  However, this may itself interfere with
+  other parts of your program.  If this is a problem, simply include
+  <windows.h> before including this file.  (Note that if you do so without
+  the WIN32_LEAN_AND_MEAN switch, some Xmlrpc-c header files won't work and
+  when your compile fails, you'll have to make a choice).
+
+  This is a sloppy way to deal with this problem.  The clean way, given that
+  Microsoft made <windows.h> much too large and screwed up the introduction
+  of Winsock2, would be for this file not to include <windows.h> at all and
+  simply prerequire the user to do it.  But it's too late to do that, as
+  lots of existing programs would stop compiling.
+============================================================================*/
 #ifndef GIRMEM_HPP_INCLUDED
 #define GIRMEM_HPP_INCLUDED
 
@@ -20,9 +41,13 @@
 #if XMLRPC_HAVE_PTHREAD
 #  include <pthread.h>
    typedef pthread_mutex_t girmem_lock;
-#else
+#elif XMLRPC_HAVE_WINTHREAD
+   /* See above about how this might interfere with the including program */
+#  define WIN32_LEAN_AND_MEAN
 #  include <windows.h>
    typedef CRITICAL_SECTION girmem_lock;
+#else
+#  error No inter-thread locking method found
 #endif
 
 /*
