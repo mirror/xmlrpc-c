9846f462a4b5d873fa3343af9f829262f9c641b5
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2012-09-08 17:33:33 +0000

    Release 01.31.05
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2389 98333e67-4a24-44d7-a75c-e53540dd3050

--- advanced/test/cpp/test.cpp
+++ advanced/test/cpp/test.cpp
@@ -9,7 +9,6 @@
 
 #include "xmlrpc-c/girerr.hpp"
 using girerr::error;
-#include "transport_config.h"
 #include "xmlrpc-c/base.hpp"
 #include "xmlrpc-c/oldcppwrapper.hpp"
 #include "xmlrpc-c/registry.hpp"
