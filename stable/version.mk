219ad5f27d2b07ac3d5bc54435cff8fa9c589d44
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2013-03-29 06:23:05 +0000

    Release 1.25.23
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2485 98333e67-4a24-44d7-a75c-e53540dd3050

--- stable/version.mk
+++ stable/version.mk
@@ -8,4 +8,4 @@
 ###############################################################################
 XMLRPC_MAJOR_RELEASE = 1
 XMLRPC_MINOR_RELEASE = 25
-XMLRPC_POINT_RELEASE = 22
+XMLRPC_POINT_RELEASE = 23
