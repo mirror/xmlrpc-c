6e3f80372e36b5b9bc2c9a5f7967bc35b26f1457
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2015-03-06 22:39:27 +0000

    Don't try to test Abyss if user didn't build Abyss
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2706 98333e67-4a24-44d7-a75c-e53540dd3050

---
+++ trunk/test/server_abyss_dummy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "server_abyss.h"
+
+
+
+void 
+test_server_abyss(void) {
+
+    printf("Running dummy Abyss XML-RPC server test\n");
+
+    printf("\n");
+    printf("Abyss XML-RPC server tests done.\n");
+}
