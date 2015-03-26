ed05401c771c760476e96039df9a82f1c462be0e
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2015-03-26 15:43:40 +0000

    Change release number to 1.41.00
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@2717 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/version.mk
+++ trunk/version.mk
@@ -7,5 +7,5 @@
 #
 ###############################################################################
 XMLRPC_MAJOR_RELEASE = 1
-XMLRPC_MINOR_RELEASE = 40
-XMLRPC_POINT_RELEASE = 99
+XMLRPC_MINOR_RELEASE = 41
+XMLRPC_POINT_RELEASE = 0
