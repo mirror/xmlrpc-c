fb4f913791c3756f0ae0b86816fc729c93f9eb9e
stickz <stickman002@mail.com>
2021-12-30 23:17:02 -0500

    Implement curl_multi_wait

--- advanced/lib/curl_transport/curlmulti.h
+++ advanced/lib/curl_transport/curlmulti.h
@@ -17,7 +17,6 @@ curlMulti_destroy(curlMulti * const curlMultiP);
 void
 curlMulti_perform(xmlrpc_env * const envP,
                   curlMulti *  const curlMultiP,
-                  bool *       const immediateWorkToDoP,
                   int *        const runningHandlesP);
 
 void
@@ -33,19 +32,5 @@ void
 curlMulti_getMessage(curlMulti * const curlMultiP,
                      bool *      const endOfMessagesP,
                      CURLMsg *   const curlMsgP);
-
-void
-curlMulti_fdset(xmlrpc_env * const envP,
-                curlMulti *  const curlMultiP,
-                fd_set *     const readFdSetP,
-                fd_set *     const writeFdSetP,
-                fd_set *     const exceptFdSetP,
-                int *        const maxFdP);
-
-void
-curlMulti_updateFdSet(curlMulti * const curlMultiP,
-                      fd_set      const readFdSet,
-                      fd_set      const writeFdSet,
-                      fd_set      const exceptFdSet);
 
 #endif
