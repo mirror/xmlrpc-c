77a4125f9b74f3188fd1f7beda34df3e6ab959d2
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2024-09-24 18:50:33 +0000

    change release number
    git-svn-id: http://svn.code.sf.net/p/xmlrpc-c/code@3244 98333e67-4a24-44d7-a75c-e53540dd3050

--- trunk/version.mk
+++ trunk/version.mk
@@ -7,5 +7,5 @@
 #
 ###############################################################################
 XMLRPC_MAJOR_RELEASE = 1
-XMLRPC_MINOR_RELEASE = 63
-XMLRPC_POINT_RELEASE = 99
+XMLRPC_MINOR_RELEASE = 64
+XMLRPC_POINT_RELEASE = 0
